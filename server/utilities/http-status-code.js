45e35f6f1f284db9a51f86665431215fcfd7ca2f
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2023-10-17 13:04:44 +0800

    :sparkles: add reusable utilities for http status code

---
+++ server/utilities/http-status-code.js
@@ -0,0 +1,15 @@
+const HTTP_STATUS_CODES = {
+  OK: 200,
+
+  CREATED: 201,
+
+  BAD_REQUEST: 400,
+
+  UNPROCESSABLE_ENTITY: 422,
+
+  INTERNAL_SERVER_ERROR: 500,
+}
+
+module.exports = {
+  HTTP_STATUS_CODES,
+}
